8857216553a8d211d9e55533f82a3c88b84a83ea
Kevin Warren <hello@kevinwarren.co.uk>
2013-08-16 19:57:09 +0100

    Update to version 1.1.0
    Updated to Bootstrap 3 RC2 front-end & lessphp 0.4.0 & yuicompressor 2.4.8

--- pie/pie.php
+++ pie/pie.php
@@ -1,7 +1,7 @@
 <?php
 /*!
- * Pie v1.0.2
- * Last Edit 22/04/2013
+ * Pie v1.1.0
+ * Last Edit 16/08/2013
  *
  * Copyright (c) 2013 Kevin Warren
  * Licensed under the The MIT License (MIT)
@@ -17,8 +17,8 @@ require_once 'yuicompressor.php';
 # pie class
 class pie {
 	
-	# set current yuicompressor.jar file - http://yui.github.io/yuicompressor/
-	private $jarPath = 'pie/yuicompressor-2.4.7.jar';
+	# set current yuicompressor.jar file - http://yui.github.io/yuicompressor/ - http://tml.github.io/yui/
+	private $jarPath = 'pie/yuicompressor-2.4.8.jar';
 	
 	# set temp path directory for php-yui-compressor
 	private $tempPath = 'pie/temp/';
@@ -27,8 +27,7 @@ class pie {
 	private $options = array('comments'=>'', 'cssOutputPath'=>'assets/css/pie.min.css', 'jsOutputPath'=>'assets/css/pie.min.js', 'linebreak'=>false, 'verbose'=>false, 'nomunge'=>false, 'semi'=>false, 'nooptimize'=>false);
 	
 	private $less;
-	private $yuiCss;
-	private $yuiJs;
+	private $yui;
 	private $css = '';
 	private $js = '';
 	private $cssMin = '';
@@ -38,14 +37,14 @@ class pie {
 	public function __construct($options = array()) {
 		
 		echo '<!DOCTYPE html><html lang="en"><head><title>Pie - The less, css &amp; js, compiler &amp; compressor</title>';
-		echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
-		echo '<meta name="viewport" content="width=device-width, initial-scale=1.0" />';
-		#echo '<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css">';
-		echo '<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/2.3.1/spacelab/bootstrap.min.css">';
+		echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" />';
+		echo '<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc2/css/bootstrap.min.css">';
+		#echo '<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/2.3.2/spacelab/bootstrap.min.css">';
+		echo '<style>pre{margin:-15px;border-radius:0px;border:none;}</style>';
 		echo '<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->';
-		echo '</head><body><div class="container-fluid">';
+		echo '</head><body><div class="container">';
 		
-		echo '<h1 class="well">Pie - The less, css &amp; js, compiler &amp; compressor</h1>';
+		echo '<div class="page-header"><h1>Pie <small>The less, css &amp; js, compiler &amp; compressor</small></h1></div>';
 		
 		# set options if argument is passed
 		foreach ($options as $option => $value) {
@@ -55,13 +54,13 @@ class pie {
 		$error = false;
 		# check yuicompressor.jar file exists
 		if (!is_file($this->jarPath)) {
-			echo '<div class="alert alert-error"><strong>Fatal Error!</strong> ' . $this->jarPath . ', file not found.</div>';
+			echo '<div class="alert alert-danger"><strong>Fatal Error!</strong> ' . $this->jarPath . ', file not found.</div>';
 			$error = true;
 		}
 		# check temp path is writable
 		$testFile = $this->tempPath . md5(mt_rand()) . '.test';
 		if (file_put_contents($testFile, 'test') === false) {
-			echo '<div class="alert alert-error"><strong>Fatal Error!</strong> ' . $this->tempPath . ', is not writeable.</div>';
+			echo '<div class="alert alert-danger"><strong>Fatal Error!</strong> ' . $this->tempPath . ', is not writeable.</div>';
 			$error = true;
 		} else {
 			unlink($testFile);
@@ -70,26 +69,26 @@ class pie {
 		if (!is_file($this->options['cssOutputPath'])) {
 			$testFile = $this->options['cssOutputPath'];
 			if (file_put_contents($testFile, 'test') === false) {
-				echo '<div class="alert alert-error"><strong>Fatal Error!</strong> ' . $this->options['cssOutputPath'] . ', is not writeable.</div>';
+				echo '<div class="alert alert-danger"><strong>Fatal Error!</strong> ' . $this->options['cssOutputPath'] . ', is not writeable.</div>';
 				$error = true;
 			} else {
 				unlink($testFile);
 			}
 		} elseif (!is_writable($this->options['cssOutputPath'])) {
-			echo '<div class="alert alert-error"><strong>Fatal Error!</strong> ' . $this->options['cssOutputPath'] . ', is not writeable.</div>';
+			echo '<div class="alert alert-danger"><strong>Fatal Error!</strong> ' . $this->options['cssOutputPath'] . ', is not writeable.</div>';
 			$error = true;
 		}
 		# js output path is writable
 		if (!is_file($this->options['jsOutputPath'])) {
 			$testFile = $this->options['jsOutputPath'];
 			if (file_put_contents($testFile, 'test') === false) {
-				echo '<div class="alert alert-error"><strong>Fatal Error!</strong> ' . $this->options['jsOutputPath'] . ', is not writeable.</div>';
+				echo '<div class="alert alert-danger"><strong>Fatal Error!</strong> ' . $this->options['jsOutputPath'] . ', is not writeable.</div>';
 				$error = true;
 			} else {
 				unlink($testFile);
 			}
 		} elseif (!is_writable($this->options['jsOutputPath'])) {
-			echo '<div class="alert alert-error"><strong>Fatal Error!</strong> ' . $this->options['jsOutputPath'] . ', is not writeable.</div>';
+			echo '<div class="alert alert-danger"><strong>Fatal Error!</strong> ' . $this->options['jsOutputPath'] . ', is not writeable.</div>';
 			$error = true;
 		}
 		if ($error) {
@@ -99,11 +98,8 @@ class pie {
 		# invoke lessphp class
 		$this->less = new lessc;
 		
-		# invoke php-yui-compressor classes
-		$this->options['type'] = 'css';
-		$this->yuiCss = new YUICompressor($this->jarPath, $this->tempPath, $this->options);
-		$this->options['type'] = 'js';
-		$this->yuiJs = new YUICompressor($this->jarPath, $this->tempPath, $this->options);
+		# invoke php-yui-compressor class
+		$this->yui = new YUICompressor($this->jarPath, $this->tempPath, $this->options);
 		
 		echo '<div class="alert alert-info"><strong>Info!</strong> Pie is ready for the meat &amp; veg a.k.a files.</div>';
 		
@@ -119,52 +115,95 @@ class pie {
 	}
 	
 	# add chunky meat & veg into the pie
-	public function add($file) {
+	public function add($file, $compress = true) {
 		if (is_file($file)) {
 			if (substr($file, -5) == '.less') {
-				# compile less file & return css
 				try {
-					$this->css .= ' ' . $this->less->compileFile($file);
+					# compile less file & return css
+					$this->css .= $this->less->compileFile($file);
+					# compress css if compress = true
+					if ($compress) {
+						$this->compress('css');
+					} else {
+						$this->cssMin .= $this->css;
+					}
 				} catch (exception $e) {
-					echo '<div class="alert alert-error"><strong>Fatal Error!</strong> ' . $e->getMessage() . '</div>';
+					echo '<div class="alert alert-danger"><strong>Fatal Error!</strong> ' . $e->getMessage() . '</div>';
 				}
 				echo '<div class="alert alert-info"><strong>Info!</strong> ' . $file . ' added.</div>';
 			} elseif (substr($file, -4) == '.css') {
 				# get css file contents
-				$this->css .= ' ' . file_get_contents($file);
+				$this->css .= file_get_contents($file);
+				# compress css if compress = true
+				if ($compress) {
+					$this->compress('css');
+				} else {
+					$this->cssMin .= $this->css;
+				}
 				echo '<div class="alert alert-info"><strong>Info!</strong> ' . $file . ' added.</div>';
 			} elseif (substr($file, -3) == '.js') {
 				# get js file contents
-				$this->js .= ' ' . file_get_contents($file);
+				$this->js .= file_get_contents($file);
+				# compress js if compress = true
+				if ($compress) {
+					$this->compress('js');
+				} else {
+					$this->jsMin .= $this->js;
+				}
 				echo '<div class="alert alert-info"><strong>Info!</strong> ' . $file . ' added.</div>';
 			} else {
 				echo '<div class="alert alert-warning"><strong>Warning!</strong> ' . $file . ', invalid file type.</div>';
 			}
 		} else {
-			echo '<div class="alert alert-error"><strong>Error!</strong> ' . $file . ', file not found.</div>';
+			echo '<div class="alert alert-danger"><strong>Error!</strong> ' . $file . ', file not found.</div>';
+		}
+	}
+	
+	# compress pie filling
+	private function compress($type) {
+		switch ($type) {
+		case 'css':
+			if (!empty($this->css)) {
+				# set type to css
+				$this->yui->setOption('type', 'css');
+				# add string
+				$this->yui->addString($this->css);
+				# clear css
+				$this->css = '';
+				# compress
+				$this->cssMin .= $this->yui->compress();
+			}
+			break;
+		case 'js':
+			if (!empty($this->js)) {
+				# set type to js
+				$this->yui->setOption('type', 'js');
+				# add string
+				$this->yui->addString($this->js);
+				# clear js
+				$this->js = '';
+				# compress
+				$this->jsMin .= $this->yui->compress();
+			}
+			break;
 		}
 	}
 	
 	# bake the pie!
 	public function bake() {
-		if (!empty($this->css)) {
-			# add string
-			$this->yuiCss->addString($this->css);
-			# compress
-			$this->cssMin .= $this->yuiCss->compress();
+		echo '<div class="alert alert-success"><strong>Baked!</strong> Your pie is ready!</div><div class="row"><div class="col-md-6">';
+		if (!empty($this->cssMin)) {
 			# save compressed css to file
 			file_put_contents($this->options['cssOutputPath'], $this->options['comments'] . $this->cssMin);
+			echo '<div class="panel panel-success"><div class="panel-heading"><strong>Saved! </strong>' . $this->options['cssOutputPath'] . '</div><div class="panel-body"><pre class="pre-scrollable">' . htmlspecialchars($this->options['comments'] . $this->cssMin) . '</pre></div></div>';
 		}
-		if (!empty($this->js)) {
-			# add string
-			$this->yuiJs->addString($this->js);
-			# compress
-			$this->jsMin .= $this->yuiJs->compress();
+		echo '</div><div class="col-md-6">';
+		if (!empty($this->jsMin)) {
 			# save compressed js to file
 			file_put_contents($this->options['jsOutputPath'], $this->options['comments'] . $this->jsMin);
+			echo '<div class="panel panel-success"><div class="panel-heading"><strong>Saved! </strong>' . $this->options['jsOutputPath'] . '</div><div class="panel-body"><pre class="pre-scrollable">' . htmlspecialchars($this->options['comments'] . $this->jsMin) . '</pre></div></div>';
 		}
-		echo '<div class="alert alert-success"><strong>Baked!</strong> Your pie is ready!</div>';
-		echo '</div></body></html>';
+		echo '</div></div></div></body></html>';
 	}
 }
 ?>
